6734f3608bb1fa62fd372436303981472c17c08a
IldarKunka <kavbanga2@gmail.com>
2021-02-27 15:11:15 +0300

    Hello World! My first programm java.

---
+++ java/src/com/company/Main.java
@@ -0,0 +1,29 @@
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        int a=1;
+        int b=2;
+        int c=a+b;
+        int i=1;
+        String hello="hello";
+        String world="world";
+
+        System.out.println(hello+" "+world + "!");
+
+        System.out.println("a > b " +(a > b));
+        System.out.println("a < b " +(a < b));
+
+        System.out.println(i);
+        i=i+1;
+        System.out.println(i);
+        i=i+1;
+        System.out.println(i);
+
+        System.out.println("1");
+        System.out.println("2");
+        System.out.println("3");
+	    System.out.println("Hello World");
+    }
+}
